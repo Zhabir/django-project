f1467ced6e7355de693635a81c9146118435ffe7
Zhabir <zhabir200313@gmail.com>
2025-04-18 18:01:44 +0700

    addel small changes and static files not working

--- Projects/django/sitenobels/nobels/urls.py
+++ Projects/django/sitenobels/nobels/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('cats/<slug:cat_slug>/', views.categories, name='cats'),
     path('archive/<year4:year>/', views.archive, name='archive'),
-
+    path('about/', views.about, name='about'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    
 ]
--- Projects/django/sitenobels/nobels/views.py
+++ Projects/django/sitenobels/nobels/views.py
@@ -2,11 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.shortcuts import render
 # Create your views here.
 
+data_db = [
+ {'id': 1, 'title': 'Анджелина Джоли', 'content':
+'Биография Анджелины Джоли', 'is_published': True},
+ {'id': 2, 'title': 'Марго Робби', 'content':
+'Биография Марго Робби', 'is_published': False},
+ {'id': 3, 'title': 'Джулия Робертс', 'content':
+'Биография Джулия Робертс', 'is_published': True},
+]
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+ {'title': "Добавить статью", 'url_name':
+'add_page'},
+ {'title': "Обратная связь", 'url_name':
+'contact'},
+ {'title': "Войти", 'url_name': 'login'}
+]
 
 def index(request):
-    return HttpResponse("Страница приложения nobels.")
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'posts': data_db        
+    }
+    return render(request, 'nobels/index.html', data)    
 
 
 def categories(request, cat_slug):
@@ -27,3 +48,24 @@ def archive(request, year):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def about(request):
+    data = {'title': 'О сайте'}
+    return render(request, 'nobels/about.html', data)
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
